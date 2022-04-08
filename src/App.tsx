e23f6ae10e61d9e7ba9079cda5f529a7d428159c
Nithin Biliya <nithin.biliya@yahoo.in>
2022-04-08 20:01:57 +0530

    adds path-to-regexp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { pathToRegexp } from "path-to-regexp";
 import "./App.css";
+
+const options = {};
 
 function App() {
   const [isMatch, setIsMatch] = useState(false);
-  const [route, setPath] = useState("");
-  const [path, setRoute] = useState("");
+  const [regularExp, setRegularExp] = useState("");
+  const [route, setRoute] = useState("");
+  const [path, setPath] = useState("");
 
   useEffect(() => {
     setIsMatch(testMatch(route, path));
   }, [route, path]);
 
   const testMatch = (r: string, p: string) => {
-    return r === p;
+    let regex = pathToRegexp(r, undefined, options);
+    setRegularExp(regex.toString());
+    return regex.test(p);
   };
 
   return (
@@ -24,6 +30,7 @@ function App() {
             setRoute(e.target.value);
           }}
         />
+        {route}
       </section>
       <section>
         Path -
@@ -33,7 +40,9 @@ function App() {
             setPath(e.target.value);
           }}
         />
+        {path}
       </section>
+      <section>regex - {regularExp}</section>
       <section>Match - {JSON.stringify(isMatch)}</section>
     </div>
   );
